3d821ce4f133495d14d155ad01c093287fe95876
Nguyen-Thi-Hong-Loan <66268096+Nguyen-Thi-Hong-Loan@users.noreply.github.com>
2020-07-27 12:49:41 +0700

    Loan: lap lich
    Lap lich

--- ProjectDataWareHouse/src/test/TestDW.java
+++ ProjectDataWareHouse/src/test/TestDW.java
@@ -1,17 +1,46 @@
 package test;
 
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 
 import modal.Download;
 
-public class TestDW {
-	public static void main(String[] args) {
-		Download download = new Download();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Nhập id config cần download: ");
-		int id = sc.nextInt();
-		System.out.println(download.saveDataFromFTPToLocal(id));
-		System.out.println("done");
+public class TestDW extends TimerTask {
+
+//		Download download = new Download();
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Nhập id config cần download: ");
+//		int id = sc.nextInt();
+//		System.out.println(download.saveDataFromFTPToLocal(id));
+//		System.out.println("done");
+
+	static String[] listIdConfig = null;
+
+	public static void main(String args[]) throws AddressException, MessagingException {
+		listIdConfig = args.clone();
+		TimerTask tasknew = new TestDW();
+		Timer timer = new Timer();
+
+		// scheduling the task at interval
+		timer.schedule(tasknew, 0, 30000);
+	}
+
+	@Override
+	public void run() {
+		Download scpObject = new Download();
+		for (int i = 0; i < listIdConfig.length; i++) {
+			try {
+				int id = Integer.parseInt(listIdConfig[i]);
+				scpObject.mainSCP(id);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+
+		}
 
 	}
 }
