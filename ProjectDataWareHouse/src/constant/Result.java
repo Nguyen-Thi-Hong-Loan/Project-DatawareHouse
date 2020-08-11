a2eab261b0ff4ba34dc4f269181f6b3dde1f7f88
huahong280399 <55909565+huahong280399@users.noreply.github.com>
2020-08-11 23:12:45 +0700

    complete step 3

--- ProjectDataWareHouse/src/constant/Result.java
+++ ProjectDataWareHouse/src/constant/Result.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package constant;
 
 public enum Result {
 	UPLOADED, SUCCESS, FAILED
 }
-=======
-package constant;
-
-public enum Result {
-	UPLOADED, SUCCESS, FAILED
-}
->>>>>>> eeb9bf5a9dabfd838e26887241b0e0c7175f885e
